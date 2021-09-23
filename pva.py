37f8159bb4eca010922238641247da55afc04d5a
Francesco De Carlo <decarlof@gmail.com>
2021-09-22 22:46:33 -0500

    update

--- pva.py
+++ pva.py
@@ -2,12 +2,28 @@
 from epics import PV
 import pvaccess as pva
 import time
+import numpy as np
+import matplotlib.pyplot as plt
 
-epics_pv = {}
 
-epics_pv['ImagePVAPName'] = PV('2bmbPG1:Pva1:')
-# pva type channel that contains projection and metadata
-image_pv_name = PV(epics_pvs['ImagePVAPName'].get()).get()
-epics_pvs['PvaPImage']          = pva.Channel(image_pv_name + 'Image')
-epics_pvs['PvaPDataType_RBV']   = pva.Channel(image_pv_name + 'DataType_RBV')
-pva_plugin_image = epics_pvs['PvaPImage']
+epics_pvs={}
+
+PVA_prefix = '2bmbPG1:Pva1:'
+
+pva_data      = pva.Channel(PVA_prefix + 'Image')
+pva_data_type = pva.Channel(PVA_prefix + 'DataType_RBV')
+pva_image     = pva_data.get('')
+pva_data_dict = pva_image.getStructureDict()     
+
+image     = pva_image['value']
+width     = pva_image['dimension'][0]['size']
+height    = pva_image['dimension'][1]['size']
+
+print(image[0]['ubyteValue'])
+image_np = np.reshape(image[0]['ubyteValue'], (width, height))
+print(width, height)
+print(image_np.shape)
+
+plt.imshow(image_np)
+plt.show()
+
