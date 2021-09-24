a582d0aaf140b181508afdeddd1e830a173e2983
Francesco De Carlo <decarlof@gmail.com>
2021-09-24 14:50:51 -0500

    update

---
+++ check_pv.py
@@ -0,0 +1,17 @@
+from epics import PV
+
+
+def on_change(pvname=None, value=None, char_value=None, **kw):
+
+    message = pvname + ': ' + 'value = ' + str(value) + '; char = ' + char_value 
+    print(message)
+
+shutter_permit = PV('ACIS:ShutterPermit')
+shutter_permit.add_callback(on_change)
+ops_message = PV('OPS:message1')
+ops_message.add_callback(on_change)
+
+shutter_permit_value = shutter_permit.get(as_string=True)
+ops_message_value = ops_message.get(as_string=True)
+print("Shutter Permit:", shutter_permit_value)
+print("OPS message:", ops_message_value)
