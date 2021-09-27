b05a82da6551ad8cc700287a765648c9f62b01e1
Francesco De Carlo <decarlof@gmail.com>
2021-09-27 09:44:33 -0500

    fixed char callback issue

--- callback1.py
+++ callback1.py
@@ -4,15 +4,16 @@ from epics import PV
 def on_change(pvname=None, value=None, char_value=None, **kw):
 
     message = pvname + ': ' + 'value = ' + str(value) + '; char = ' + char_value 
-    # print(message)
-    print(kw)
+    print(message)
+    # print(kw)
 
-shutter_permit = PV('ACIS:ShutterPermit')
+shutter_permit = PV('ACIS:ShutterPermit', form='ctrl')
+tt='test'
 shutter_permit.add_callback(on_change)
-ops_message = PV('OPS:message1')
-ops_message.add_callback(on_change)
+# ops_message = PV('OPS:message1')
+# ops_message.add_callback(on_change)
 
 shutter_permit_value = shutter_permit.get(as_string=True)
-ops_message_value = ops_message.get(as_string=True)
+# ops_message_value = ops_message.get(as_string=True)
 print("Shutter Permit:", shutter_permit_value)
-print("OPS message:", ops_message_value)
+# print("OPS message:", ops_message_value)
