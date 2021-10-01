0eb5411324e35f531b40fbe0e44faf44410c35a8
Francesco De Carlo <decarlof@gmail.com>
2021-09-30 20:24:38 -0500

    first commit

--- dm_slack/dmagic.py
+++ dm_slack/dmagic.py
@@ -1,54 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# #########################################################################
-# Copyright (c) 2015, UChicago Argonne, LLC. All rights reserved.         #
-#                                                                         #
-# Copyright 2015. UChicago Argonne, LLC. This software was produced       #
-# under U.S. Government contract DE-AC02-06CH11357 for Argonne National   #
-# Laboratory (ANL), which is operated by UChicago Argonne, LLC for the    #
-# U.S. Department of Energy. The U.S. Government has rights to use,       #
-# reproduce, and distribute this software.  NEITHER THE GOVERNMENT NOR    #
-# UChicago Argonne, LLC MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR        #
-# ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.  If software is     #
-# modified to produce derivative works, such modified software should     #
-# be clearly marked, so as not to confuse it with the version available   #
-# from ANL.                                                               #
-#                                                                         #
-# Additionally, redistribution and use in source and binary forms, with   #
-# or without modification, are permitted provided that the following      #
-# conditions are met:                                                     #
-#                                                                         #
-#     * Redistributions of source code must retain the above copyright    #
-#       notice, this list of conditions and the following disclaimer.     #
-#                                                                         #
-#     * Redistributions in binary form must reproduce the above copyright #
-#       notice, this list of conditions and the following disclaimer in   #
-#       the documentation and/or other materials provided with the        #
-#       distribution.                                                     #
-#                                                                         #
-#     * Neither the name of UChicago Argonne, LLC, Argonne National       #
-#       Laboratory, ANL, the U.S. Government, nor the names of its        #
-#       contributors may be used to endorse or promote products derived   #
-#       from this software without specific prior written permission.     #
-#                                                                         #
-# THIS SOFTWARE IS PROVIDED BY UChicago Argonne, LLC AND CONTRIBUTORS     #
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       #
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS       #
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL UChicago     #
-# Argonne, LLC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,        #
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,    #
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;        #
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER        #
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT      #
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN       #
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         #
-# POSSIBILITY OF SUCH DAMAGE.                                             #
-# #########################################################################
-
 """
-Module containing an example on how to use DMagic to access the APS scheduling
-system information.
+Module containing an example on how to use DMagic and Slack to create a channel named GUP-# 
+retrieving the information from the scheduling system
+
+running under python 3.8 
+
+and bash including:
+
+export DM_ROOT_DIR=/home/dm_bm/production
+export DM_DS_WEB_SERVICE_URL=https://xraydtn02.xray.aps.anl.gov:22236
+export DM_DAQ_WEB_SERVICE_URL=https://s2bmdm.xray.aps.anl.gov:33336
+export DM_CAT_WEB_SERVICE_URL=https://s2bmdm.xray.aps.anl.gov:44436
+export DM_PROC_WEB_SERVICE_URL=https://s2bmdm.xray.aps.anl.gov:55536
+export DM_APS_DB_WEB_SERVICE_URL=https://xraydtn02.xray.aps.anl.gov:11236
+export DM_STATION_NAME=2BM
+export DM_ALLOWED_EXPERIMENT_TYPES=2BM,TEST
+
+export DM_LOGIN_FILE=/home/dm_bm/etc/.user2bm.system.login
+export DM_BEAMLINE_NAME=2-BM-A,B
+export DM_ESAF_SECTOR=02
+export DM_STATION_GUI_USE_ESAF_DB=
+export DM_BEAMLINE_MANAGERS=d49734
+export DM_DATA_DIRECTORY_MAP=
 
 """
 import os
