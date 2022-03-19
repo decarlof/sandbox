1d35ab9c2730f8c18618d79bbc03834b42715c93
Francesco De Carlo <decarlof@gmail.com>
2022-03-18 22:20:17 -0600

    using json for authorization

--- webcam.py
+++ webcam.py
@@ -1,21 +1,31 @@
 import os
 import cv2
 import pathlib
+import json
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 
-env_path = os.path.join(str(pathlib.Path.home()), '.webcam')
+def read_json(fname):
+    with open(fname, 'r') as fp:
+        alist = json.load(fp)
+    return alist
+
+# Create a file in the home directory called access.json
+# {
+#     "p": "password",
+#     "u": "username"
+# }
+
+
+access_fname = os.path.join(str(pathlib.Path.home()), 'access.json')
+access_dic = read_json(access_fname)
 
 try:
-    with open(env_path,'rb') as fid:
-        print('U_KEY and P_KEY file found at %s' % env_path)
-    load_dotenv(dotenv_path=env_path)
 
-    ret, frame = cv2.VideoCapture('http://' + u_key +':' + p_secret + '@164.54.113.162/cgi-bin/mjpeg?stream=1').read()
+    ret, frame = cv2.VideoCapture('http://' + access_dic['u'] +':' + access_dic['p'] + '@164.54.113.162/cgi-bin/mjpeg?stream=1').read()
     plt.imshow(frame)
     plt.show()
 
 except FileNotFoundError:
-    log.error('U_KEY and P_KEY are missing. Set them in a file called: %s' % env_path)
-    exit()
+    log.error('username and password are missing. Set them in a file called: %s' % access_fname )
 
