e7c0caca377f219b10a08e67196bf393b07080eb
Francesco De Carlo <decarlof@gmail.com>
2022-03-22 15:51:15 -0600

    first commit

--- webcam.py
+++ webcam.py
@@ -23,8 +23,9 @@ access_dic = read_json(access_fname)
 try:
 
     ret, frame = cv2.VideoCapture('http://' + access_dic['u'] +':' + access_dic['p'] + '@164.54.113.162/cgi-bin/mjpeg?stream=1').read()
-    plt.imshow(frame)
-    plt.show()
+    if ret:
+    	plt.imshow(frame)
+    	plt.show()
 
 except FileNotFoundError:
     log.error('username and password are missing. Set them in a file called: %s' % access_fname )
