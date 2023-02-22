0c8e810419d8a8b59c62e2e93525411b11be95d2
Viktor Nikitin <vnikitin@anl.gov>
2023-02-22 14:59:57 -0600

    tested at 2-BM

--- hdf/hdf_merge_02.py
+++ hdf/hdf_merge_02.py
@@ -15,35 +15,36 @@ try:
             os.makedirs(out_dir)
 
         h5_file_list = list(filter(lambda x: x.endswith(('.h5', '.hdf', 'hdf5')), os.listdir(top)))
-        print(h5_file_list, top)
+        # print(h5_file_list, top)
         if (h5_file_list):
             h5_file_list.sort()
-            print("Found: %s" % h5_file_list) 
+            # print("Found: %s" % h5_file_list) 
             index=0
             for fname in h5_file_list:
-
-                try:  # trying to copy meta
-                    tree, meta_dict = meta.read_hdf(fname)
-                    with h5py.File(fname,'r') as f:
-                        for key, value in meta_dict.items():
-                            # print(key, value)
-                            dset = rec_virtual.create_dataset(key, data=value[0], dtype=f[key].dtype, shape=(1,))
-                            if value[1] is not None:
-                                s = value[1]
-                                utf8_type = h5py.string_dtype('utf-8', len(s)+1)
-                                dset.attrs['units'] =  np.array(s.encode("utf-8"), dtype=utf8_type)
-                except:
-                    log.info('Skip copying meta')
-                    pass
+                print("Found: %s" % fname) 
 
                 with h5py.File(os.path.join(out_dir, fname), 'a') as h5w:
                     with h5py.File(fname, 'r') as h5r:
-                        print('Reading: ', fname)
+                        try:  # trying to copy meta
+                            tree, meta_dict = meta.read_hdf(fname)
+                            print('Reading meta data: ', fname)
+                            for key, value in meta_dict.items():
+                                # print(key, value)
+                                dset = h5w.create_dataset(key, data=value[0], dtype=h5r[key].dtype, shape=(1,))
+                                if value[1] is not None:
+                                    s = value[1]
+                                    utf8_type = h5py.string_dtype('utf-8', len(s)+1)
+                                    dset.attrs['units'] =  np.array(s.encode("utf-8"), dtype=utf8_type)
+                        except:
+                            print('ERROR: Skip copying meta')
+                            pass
+
+                        print('Reading data: ', fname)
                         ds_arr = h5r['exchange/data'][...]
                         print ('Saving', ':', ds_arr.dtype, ds_arr.shape, 'to: ', os.path.join(out_dir, fname))
                         h5w.create_dataset('exchange/data', data=ds_arr[:])
-
-
+        else:
+            print('ERROR: no hdf file found in %s' % top)
     else:
         print("Path: %s does not exist" % top)
 
