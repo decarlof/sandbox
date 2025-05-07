9c2dfba63c680121c511460a7c178f131894bad9
Francesco De Carlo <decarlof@gmail.com>
2025-05-07 09:58:26 -0500

    first commit

---
+++ hdf/AddingTheta.py
@@ -0,0 +1,15 @@
+import dxfile.dxtomo as dx
+import numpy as np
+
+n_proj = 512 # enter the correct number of projections
+fname = enter full path to the hdf file
+
+# Theta
+theta_step = np.pi / n_proj
+theta_step_deg = theta_step * 180./np.pi
+theta = np.arange(0, 180., 180. / n_proj)
+
+f = dx.File(fname, mode='w')
+f.add_entry(dx.Entry.data(theta={'value': theta, 'units':'degrees'}))
+
+f.close()
