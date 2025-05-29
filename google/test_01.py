47088a0e647002846394e0ae08a085a3e51eea34
Francesco De Carlo <decarlof@gmail.com>
2025-05-29 16:24:46 -0500

    crated google_drive class

--- google/test_01.py
+++ google/test_01.py
@@ -8,6 +8,65 @@ from googleapiclient.http import MediaFileUpload
 GOOGLE_TOKEN = os.path.join(str(pathlib.Path.home()), 'tokens', 'google_token.json')
 
 
+class DriveSnippets(object):
+    def __init__(self, service, credentials):
+        self.service = service
+        self.credentials = credentials
+
+    def upload_file(self, drive_service, filepath, mimetype, drive_filename, parent_folder_id=None):
+        file_metadata = {'name': drive_filename}
+        if parent_folder_id:
+            file_metadata['parents'] = [parent_folder_id]
+
+        media = MediaFileUpload(filepath, mimetype=mimetype)
+        file = drive_service.files().create(
+            body=file_metadata,
+            media_body=media,
+            fields='id'
+        ).execute()
+        print('File uploaded. File ID:', file.get('id'))
+        
+    def find_file_id(self, drive_service, filename, parent_folder_id=None):
+        query = f"name='{filename}'"
+        if parent_folder_id:
+            query += f" and '{parent_folder_id}' in parents"
+        results = drive_service.files().list(
+            q=query,
+            spaces='drive',
+            fields='files(id, name)',
+            pageSize=1
+        ).execute()
+        files = results.get('files', [])
+        if files:
+            return files[0]['id']
+        return None
+
+    def upload_or_update_file(self, drive_service, filepath, mimetype, drive_filename, parent_folder_id=None):
+        file_id = self.find_file_id(drive_service, drive_filename, parent_folder_id)
+        media = MediaFileUpload(filepath, mimetype=mimetype)
+
+        if file_id:
+            print(f"File '{drive_filename}' exists (ID: {file_id}), updating...")
+            updated_file = drive_service.files().update(
+                fileId=file_id,
+                media_body=media
+            ).execute()
+            print('File updated:', updated_file.get('id'))
+            return updated_file.get('id')
+        else:
+            print(f"File '{drive_filename}' does not exist, creating...")
+            file_metadata = {'name': drive_filename}
+            if parent_folder_id:
+                file_metadata['parents'] = [parent_folder_id]
+            created_file = drive_service.files().create(
+                body=file_metadata,
+                media_body=media,
+                fields='id'
+            ).execute()
+            print('File created:', created_file.get('id'))
+            return created_file.get('id')
+
+
 class SlidesSnippets(object):
     def __init__(self, service, credentials):
         self.service = service
@@ -235,32 +294,18 @@ class SlidesSnippets(object):
         return response
 
 def google_drive(token_fname):
-
     print('Establishing connection to google drive')
     try:
         creds = service_account.Credentials.from_service_account_file(token_fname).with_scopes(['https://www.googleapis.com/auth/presentations']).with_scopes(['https://www.googleapis.com/auth/drive'])  # FULL DRIVE SCOPE
         drive = build('drive', 'v3', credentials=creds)
+        snippets_drive = DriveSnippets(drive, creds)
         print('Connection to google drive: OK')
-        return drive
+        return snippets_drive, drive
     except Exception as e:
         print('Failed to connect to Google Drive:', e)
         return None
 
-def upload_file(drive_service, filepath, mimetype, drive_filename, parent_folder_id=None):
-    file_metadata = {'name': drive_filename}
-    if parent_folder_id:
-        file_metadata['parents'] = [parent_folder_id]
-
-    media = MediaFileUpload(filepath, mimetype=mimetype)
-    file = drive_service.files().create(
-        body=file_metadata,
-        media_body=media,
-        fields='id'
-    ).execute()
-    print('File uploaded. File ID:', file.get('id'))
-
 def google_slide(token_fname):
-
     print('Establishing connection to google slide')
     try:
         creds = service_account.Credentials.from_service_account_file(token_fname).with_scopes(['https://www.googleapis.com/auth/presentations'])
@@ -288,47 +333,6 @@ def init_slide(google, presentation_url, file_name):
     return presentation_id, page_id
 
 
-def find_file_id(drive_service, filename, parent_folder_id=None):
-    query = f"name='{filename}'"
-    if parent_folder_id:
-        query += f" and '{parent_folder_id}' in parents"
-    results = drive_service.files().list(
-        q=query,
-        spaces='drive',
-        fields='files(id, name)',
-        pageSize=1
-    ).execute()
-    files = results.get('files', [])
-    if files:
-        return files[0]['id']
-    return None
-
-def upload_or_update_file(drive_service, filepath, mimetype, drive_filename, parent_folder_id=None):
-    file_id = find_file_id(drive_service, drive_filename, parent_folder_id)
-    media = MediaFileUpload(filepath, mimetype=mimetype)
-
-    if file_id:
-        print(f"File '{drive_filename}' exists (ID: {file_id}), updating...")
-        updated_file = drive_service.files().update(
-            fileId=file_id,
-            media_body=media
-        ).execute()
-        print('File updated:', updated_file.get('id'))
-        return updated_file.get('id')
-    else:
-        print(f"File '{drive_filename}' does not exist, creating...")
-        file_metadata = {'name': drive_filename}
-        if parent_folder_id:
-            file_metadata['parents'] = [parent_folder_id]
-        created_file = drive_service.files().create(
-            body=file_metadata,
-            media_body=media,
-            fields='id'
-        ).execute()
-        print('File created:', created_file.get('id'))
-        return created_file.get('id')
-
-
 def main():
 
     snippets_slide = google_slide(GOOGLE_TOKEN)
@@ -338,12 +342,12 @@ def main():
     presentation_id, page_id = init_slide(snippets_slide, presentation_url, file_name)
     descr = "test text box with bullet"
     snippets_slide.create_textbox_with_bullets(presentation_id, page_id, descr, 240, 120, 0, 18, 8, 0)
-    drive_service = google_drive(GOOGLE_TOKEN)
-
-    if drive_service:
-        # Example: Upload a text file
+    
+    snippets_drive, drive_service = google_drive(GOOGLE_TOKEN)
+    if snippets_drive:
+        # Example: Upload image
         folder_id = '1aVGsEXgxM1IPO9ZdSGUl4jkl6yGB-llT'
-        file_id = upload_or_update_file(
+        file_id = snippets_drive.upload_or_update_file(
             drive_service=drive_service,
             filepath='AD_02.png',
             # mimetype='text/plain',
@@ -352,7 +356,6 @@ def main():
             parent_folder_id=folder_id
         )
     
-    # image_url = f"https://drive.google.com/file/d/{file_id}/view"
     image_url = f"https://drive.google.com/uc?export=view&id={file_id}"
     print("File URL:", image_url)
     snippets_slide.create_image(presentation_id, page_id, image_url, 300, 300, 0, 0)
