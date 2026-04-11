f376fc04d4b99ccd020ab3a9ff5cccb49d0b225d
Francesco De Carlo <decarlof@gmail.com>
2026-04-11 17:41:47 -0500

    added tools to add meta data to merged file

---
+++ merge_scans/copy_meta.py
@@ -0,0 +1,17 @@
+import h5py
+src = '/data2/2BM/2026-04-Herrera-1020972/VPPLT-1964_Y-18mm_007.h5'
+dst = '/data2/2BM/2026-04-Herrera-1020972/VPPLT-1964_Y-18mm_merged.h5'
+
+
+groups = ['/process', '/measurement', '/defaults']
+
+with h5py.File(src, 'r') as s:
+    with h5py.File(dst, 'a') as d:
+        for g in groups:
+            if g in d:
+                print('Skipping ' + g + ' (already exists)')
+            elif g in s:
+                s.copy(g, d)
+                print('Copied ' + g)
+            else:
+                print('Not found in source: ' + g)
